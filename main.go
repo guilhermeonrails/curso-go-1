a05eabcd680323440a728e2c97a0f785221d4faa
guilhermelima <guilhermelimaruby@gmail.com>
2024-07-10 19:03:36 -0300

    1.4

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	var nomePizzaria string = "Pizzaria Go"
 	instagram, telefone := "@pizzaria_go", 11951
-	fmt.Println(nomePizzaria, instagram, telefone)
+	fmt.Printf("Nome da pizzaria: %s (instagram: %s) - Telefone: %d", nomePizzaria, instagram, telefone)
 }
