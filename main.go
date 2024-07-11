5d0a5cc14692bf39b21ec399b0e4c0f843d4fcdc
guilhermelima <guilhermelimaruby@gmail.com>
2024-07-11 17:42:40 -0300

    2.3

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 type Pizza struct {
 	ID    int
@@ -9,11 +11,18 @@ type Pizza struct {
 }
 
 func main() {
+	router := gin.Default()
+	router.GET("/pizzas", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"pizzas": "Toscana, Calabresa sem cebola",
+		})
+	})
 	// toscana, Marguerita, atum com queijo
-	var pizzas = []Pizza{
-		{ID: 1, nome: "Toscana", preco: 49.5},
-		{ID: 2, nome: "Marguerita", preco: 79.5},
-		{ID: 3, nome: "Atum com queijo", preco: 69.5},
-	}
-	fmt.Println(pizzas)
+	// var pizzas = []Pizza{
+	// 	{ID: 1, nome: "Toscana", preco: 49.5},
+	// 	{ID: 2, nome: "Marguerita", preco: 79.5},
+	// 	{ID: 3, nome: "Atum com queijo", preco: 69.5},
+	// }
+	// fmt.Println(pizzas)
+	router.Run()
 }
