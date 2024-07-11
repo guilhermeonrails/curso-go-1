20f8203328af3b19ac6b0123e3b69bdc0bd3d02b
guilhermelima <guilhermelimaruby@gmail.com>
2024-07-11 17:32:03 -0300

    2.2

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ type Pizza struct {
 }
 
 func main() {
-	var nomePizzaria string = "Pizzaria Go"
-	var toscana Pizza
-	instagram, telefone := "@pizzaria_go", 11951
-	fmt.Println(nomePizzaria, instagram, telefone)
+	// toscana, Marguerita, atum com queijo
+	var pizzas = []Pizza{
+		{ID: 1, nome: "Toscana", preco: 49.5},
+		{ID: 2, nome: "Marguerita", preco: 79.5},
+		{ID: 3, nome: "Atum com queijo", preco: 69.5},
+	}
+	fmt.Println(pizzas)
 }
