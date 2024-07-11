88248bb8b28b32bce3d3789670ef46a2e3546b2a
guilhermelima <guilhermelimaruby@gmail.com>
2024-07-11 17:14:05 -0300

    2.1

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+type Pizza struct {
+	ID    int
+	nome  string
+	preco float64
+}
+
 func main() {
 	var nomePizzaria string = "Pizzaria Go"
+	var toscana Pizza
 	instagram, telefone := "@pizzaria_go", 11951
-	fmt.Printf("Nome da pizzaria: %s (instagram: %s) - Telefone: %d", nomePizzaria, instagram, telefone)
+	fmt.Println(nomePizzaria, instagram, telefone)
 }
